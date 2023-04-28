da908eb8b8ef9ded67227b32c0a33d399e9ebf37
Daniel Anestopoulos <dstops97@gmail.com>
2023-04-28 11:06:43 -0400

    Add global variables + operate function

--- math.js
+++ math.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 const add = function (num1, num2){
     return num1 + num2;
 }
@@ -12,4 +16,19 @@ const multiply = function (num1, num2){
 
 const divide = function (num1, num2){
     return num1 / num2;
+}
+
+const operate = function (num1, num2, operator){
+    if(operator == '+'){
+        return add(num1, num2);
+    }
+    if(operator == '-'){
+        return subtract(num1, num2);
+    }
+    if(operator == '*'){
+        return multiply(num1, num2);
+    }
+    if(operator == '/'){
+        return divide(num1, num2);
+    }
 }
