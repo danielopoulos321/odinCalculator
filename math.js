299ede9197b91683bddf99773e55250024fe1b48
Daniel Anestopoulos <dstops97@gmail.com>
2023-05-04 16:34:45 -0400

    Fix math logic & null errors

--- math.js
+++ math.js
@@ -1,24 +1,77 @@
 let num1;
 let num2;
-let operator;
+let operator = '';
+let displayNum = '';
 
-const add = function (num1, num2){
+const screen = document.getElementById('screen');
+const numKeys = document.getElementsByClassName('number');
+const clearKey = document.getElementById('clear');
+const oppKeys = document.getElementsByClassName('opp');
+const equalKey = document.getElementById('equals');
+
+for (i of numKeys) {
+    i.addEventListener('click', function() {
+      updateScreen(this.textContent);
+    });
+  }
+
+for (i of oppKeys) {
+    i.addEventListener('click', function() {
+    if (operator == ''){
+        num1 = parseInt(displayNum);
+        operator = this.textContent;
+        displayNum = '';
+    } else {
+        equals();
+        num1 = parseInt(displayNum);
+        operator = this.textContent;
+        displayNum = '';
+    }});
+  }
+
+clearKey.addEventListener('click', () => reset());
+equalKey.addEventListener('click', () => {
+    equals(); 
+    operator = ''
+});
+
+function reset(){
+    displayNum = '';
+    operator = '';
+    screen.textContent = 0;
+}
+
+function updateScreen(num){
+    displayNum += num;
+    screen.textContent = displayNum;
+}
+
+function equals(){
+    if(num1 && operator != ''){
+        num2 = parseInt(displayNum);
+        displayNum = operate(num1, num2, operator);
+        screen.textContent = displayNum;
+        num1 = parseInt(displayNum);
+    }
+}
+
+function add(num1, num2){
     return num1 + num2;
 }
 
-const subtract = function (num1, num2){
+function subtract(num1, num2){
     return num1 - num2;
 }
 
-const multiply = function (num1, num2){
+function multiply(num1, num2){
     return num1 * num2;
 }
 
-const divide = function (num1, num2){
+function divide(num1, num2){
     return num1 / num2;
 }
 
-const operate = function (num1, num2, operator){
+function operate(num1, num2, operator){
     if(operator == '+'){
         return add(num1, num2);
     }
