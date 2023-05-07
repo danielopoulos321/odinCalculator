f6541181c7c3247e95976601c56baf0dfa9c7b8c
Daniel Anestopoulos <dstops97@gmail.com>
2023-05-07 01:00:47 -0400

    Fix Nan due to decimal

--- math.js
+++ math.js
@@ -9,38 +9,67 @@ const numKeys = document.getElementsByClassName('number');
 const clearKey = document.getElementById('clear');
 const oppKeys = document.getElementsByClassName('opp');
 const equalKey = document.getElementById('equals');
+const decimalKey = document.getElementById('decimal');
 
 for (i of numKeys) {
     i.addEventListener('click', function() {
       updateScreen(this.textContent);
       operatorPressed = false;
+      enableOpps();
     });
   }
 
 for (i of oppKeys) {
     i.addEventListener('click', function() {
+        enableOpps();
     if (operator == ''){
         num1 = parseFloat(screen.textContent);
         operator = this.textContent;
         displayNum = '';
         operatorPressed = true;
+        this.disabled = true;
     } else {
         if(operatorPressed){
             operator = this.textContent;
+            this.disabled = true;
+        } else if (!operatorPressed && displayNum ==''){
+            num1 = parseFloat(screen.textContent);
+            operator = this.textContent;
+            displayNum = '';
+            operatorPressed = true;
+            this.disabled = true;
         } else {
             equals();
             num1 = parseFloat(screen.textContent);
             operator = this.textContent;
             displayNum = '';
+            this.disabled = true;
         }
     }});
   }
 
 clearKey.addEventListener('click', () => reset());
 equalKey.addEventListener('click', () => {
+    enableOpps();
+    if(screen.textContent == '0.'){
+        displayNum = 0;
+        screen.textContent = displayNum;
+    }
     if(!operatorPressed){
         equals(); 
         operator = '';
+    }
+});
+decimalKey.addEventListener('click', () => {
+    if(screen.textContent == '0'){
+        displayNum = '0.';
+        screen.textContent = displayNum;
+    } else if(screen.textContent.includes('.')){
+        return;
+    } else if(displayNum == ''){
+        updateScreen('0.');
+    } else {
+        updateScreen('.');
     }
 });
 
@@ -48,10 +77,11 @@ function reset(){
     displayNum = '';
     operator = '';
     screen.textContent = 0;
+    enableOpps();
 }
 
 function updateScreen(num){
-    if(this.textContent == 0 && num == 0){
+    if(screen.textContent == 0 && num == 0){
         return;
     } else if(displayNum.length > 15){
         screen.textContent = parseFloat(displayNum).toExponential(10);
@@ -63,11 +93,18 @@ function updateScreen(num){
 }
 
 function equals(){
+    if(screen.textContent == '0.'){
+        screen.textContent = 0;
+        displayNum = '';
+    }
     if(num1 && operator != ''){
         num2 = parseFloat(displayNum);
-        displayNum = roundResult(operate(num1, num2, operator));
+        displayNum = round(operate(num1, num2, operator));
         screen.textContent = displayNum;
         num1 = parseFloat(displayNum);
+        displayNum = '';
+    }
+    if(!num1 && !num2){
         displayNum = '';
     }
 }
@@ -81,7 +118,7 @@ function subtract(num1, num2){
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return parseFloat(num1 * num2).toExponential(10);
 }
 
 function divide(num1, num2){
@@ -103,6 +140,12 @@ function operate(num1, num2, operator){
     }
 }
 
-function roundResult(num) {
-    return Math.round(num * 1000) / 1000
-  }
+function round(num){
+    return Math.round(num * 1000000)/1000000;
+}
+
+function enableOpps(){
+    for (button of oppKeys){
+        button.disabled = false;
+    }
+}
